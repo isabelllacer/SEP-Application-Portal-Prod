8edd40e0036f6a6ec666a841ea56b20feb98ef09
isabelllacer <isabelllacer@yahoo.com>
2019-01-22 13:02:32 -0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,75 @@
+import React, { Component } from 'react';
+import './App.css';
+import firebase from './firebase.js';
+
+class Entry extends React.Component {
+  render() {
+    const status = this.props.status ?
+        ' status' :
+        '';
+    return (
+        <div className={'entry' + status}>{this.props.value}</div>
+    );
+  }
+}
+
+class Row extends React.Component {
+  render() {
+    return (
+      <div key={this.props.info.id} className='row'>
+        {<Entry value={this.props.info.name}/>}
+        {<Entry status={true} value={this.props.info.status}/>}
+        {<Entry value={this.props.info.major}/>}
+        {<Entry value={this.props.info.year}/>}
+      </div>
+    );
+  }
+}
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      items: []
+    }
+  }
+
+  componentDidMount() {
+    const itemsRef = firebase.database().ref('applicants');
+    itemsRef.on('value', (snapshot) => {
+      let items = snapshot.val();
+      let newState = [];
+      for (let item in items) {
+        newState.push({
+          id: item,
+          name: items[item].name,
+          status: items[item].status,
+          major: items[item].major,
+          year: items[item].year
+        });
+      }
+      this.setState({
+        items: newState
+      });
+    });
+  }
+
+  render() {
+    return (
+      <div className='app'>
+        <header>
+            <div className='wrapper'>
+              <h1>SEP Application Portal</h1>
+            </div>
+        </header>
+        <div className='table'>
+                {this.state.items.map((item) => {
+                  return <Row info={item}/>; //for iteration: could include "fields" prop w list of columns and adapt info into a string dictionary
+                })}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+import firebase from 'firebase'
+const config = {
+    apiKey: "AIzaSyCF5PmdfG2qfgsG6SWgN06t91nfUdTS-Dc",
+    authDomain: "fun-food-friends-e2658.firebaseapp.com",
+    databaseURL: "https://fun-food-friends-e2658.firebaseio.com",
+    projectId: "fun-food-friends-e2658",
+    storageBucket: "fun-food-friends-e2658.appspot.com",
+    messagingSenderId: "788527700096"
+  };
+  firebase.initializeApp(config);
+  export default firebase;
