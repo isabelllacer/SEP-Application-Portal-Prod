189c616d4e4b11ff71ab4df1c5096f982a865545
isabelllacer <isabelllacer@yahoo.com>
2019-02-01 13:56:43 -0800

    add sign out automatically

--- src/Main.js
+++ src/Main.js
@@ -1,10 +1,33 @@
 import React from 'react';
 import './Main.css';
+import firebase from './firebase.js';
 import { Link, HashRouter, Route, Switch } from 'react-router-dom';
 import View from "./View";
 import App from "./App";
 
 class Main extends React.Component {
+  constructor(props, context) {
+      super(props, context);
+      this.componentCleanup = this.componentCleanup.bind(this);
+  }
+
+  componentCleanup() {
+    firebase.auth().signOut().then(function() {
+      console.log('Signed Out');
+    }, function(error) {
+      console.error('Sign Out Error', error);
+    });
+  }
+
+  componentDidMount(){
+    window.addEventListener('beforeunload', this.componentCleanup);
+  }
+
+  componentWillUnmount() {
+        this.componentCleanup();
+        window.removeEventListener('beforeunload', this.componentCleanup); // remove the event handler for normal unmounting
+    }
+
   render() {
     return (
         <div className='app'>
