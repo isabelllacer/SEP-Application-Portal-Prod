0d1cd3a088ad3ceec9c3dd93d0a0ed34ef4a8e96
isabelllacer <isabelllacer@yahoo.com>
2019-02-01 15:31:02 -0800

    change firebase reference

--- src/firebase.js
+++ src/firebase.js
@@ -1,11 +1,11 @@
 import firebase from 'firebase'
 const config = {
-    apiKey: "AIzaSyCF5PmdfG2qfgsG6SWgN06t91nfUdTS-Dc",
-    authDomain: "fun-food-friends-e2658.firebaseapp.com",
-    databaseURL: "https://fun-food-friends-e2658.firebaseio.com",
-    projectId: "fun-food-friends-e2658",
-    storageBucket: "gs://fun-food-friends-e2658.appspot.com/",
-    messagingSenderId: "788527700096"
+    apiKey: "AIzaSyBtB9unluhFAOusu6KYFY98c-Bc5p6lOtU",
+    authDomain: "sep-apps-sp19.firebaseapp.com",
+    databaseURL: "https://sep-apps-sp19.firebaseio.com",
+    projectId: "sep-apps-sp19",
+    storageBucket: "sep-apps-sp19.appspot.com",
+    messagingSenderId: "133570899071"
   };
   firebase.initializeApp(config);
   export default firebase;
