115ddcae3585d8f5f89ea84bae940a624da95d38
isabelllacer <isabelllacer@yahoo.com>
2019-02-01 14:28:57 -0800

    make it better

--- src/Main.js
+++ src/Main.js
@@ -13,7 +13,6 @@ class Main extends React.Component {
 
   componentCleanup() {
     firebase.auth().signOut().then(function() {
-      console.log('Signed Out');
     }, function(error) {
       console.error('Sign Out Error', error);
     });
